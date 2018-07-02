df2c051ceb95999fb788b25e588494db3a37c473
__SYN__ <aero.sayan@gmail.com>
2018-07-02 22:37:52 +0530

    Initial commit.
    Progress :
    + Created a linear regression model with ordinary least squares.
    
    Signed-off-by: __SYN__ <aero.sayan@gmail.com>

---
+++ README.md
@@ -0,0 +1,34 @@
+# Learning Machine Learning
+<i>Pun fully intended. #NO_REGRETS  </i></br>
+
+I have previously created and used Artificial Neural Networks(ANN) with the help
+of the MATLAB System Identification (essentially for regression) for
+training an ANN on simulation data of UAV landing gear shock absorbtion and
+suspension characteristics when rolling over different kind of runways and under
+multiple simulated base excitations.
+
+### So.
+I want to break apart the Machine Learning(ML) algorithms and understand them.</br>
+Moreover, we have so much amazing AI and ML toolboxes such as Tensorflow, Keras,
+scikit-learn that many people are able to create amazing things. However at the
+expense of not fully understanding the concepts involved in such libraries and
+hence not able to essentialy create optimised solutions for the problems that
+they are trying to solve.</br>
+![programmers](memes/programmers.jpg)
+
+Enough said. Let's get coding.
+
+
+
+## Author : Sayan Bhattacharjee
+## Email  : aero.sayan@gmail.com
+
+### Contents
+ML is vast. And I do not want to pollute my github repo with learning material.
+So all ML related learning and practice code will be in this repo.
+And they are...
++ src/01_soup_sale.py : Linear regression using ordinary least square
+	- Winter season temperature vs hot soup sale. Yum!
+	- We fit a line through the data points using linear regresion.
+	- RESULT </br> ![soup-img](images/01_soup_sale_05.JPG)
+	- More images in images/ folder.
---
+++ src/01_soup_sale.py
@@ -0,0 +1,37 @@
+# FILE : 01_soup_sale.py
+# AUTH : Sayan Bhattacharjee
+# EMAIL: aero.sayan@gmail.com
+# DATE : 2/JULY/2018 MON
+# INFO : How does hot soup sale change in winter based on temperature?
+#      : Here, we do linear regression with ordinary least squares
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+n = 100                                                   # no. of data points
+temp = np.linspace(3, 30, n)                              # temperature (deg C)
+noise = np.random.randint(-5,7,size = n)                 # noise to simulate RL
+soup = np.linspace(40, 22 , n, dtype = 'int') + noise     # soup sale count
+
+# We are re-assigning the data since we like to write in this form
+x = temp                                                  # x co-ordinate
+y = soup                                                  # y co-ordinate
+x_bar = sum(x)/float(n)                                   # average of x
+y_bar = sum(y)/float(n)                                   # average of y
+
+
+m = sum((x-x_bar)*(y-y_bar))/ float(sum((x-x_bar)**2) )   # slope
+b = y_bar - m*x_bar                                       # y intercept
+
+print("The linear regression has resulted in ...")
+print("m(slope)       : ",m )
+print("b(y intercept) : ",b )
+
+
+plt.scatter(x,y)
+plt.plot(x,m*x + b,'-r')
+plt.title("Temperature vs Soup Sale \n (linear regression with ordinary least squares)")
+plt.xlabel("Temp in degree Celcius")
+plt.ylabel("Hot soup bowls sold")
+plt.grid()
+plt.show()
